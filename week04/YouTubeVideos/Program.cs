4de528a9878a3ae40392319c827b5586709dbedf
jazminruiz16 <jazmingruiz@gmail.com>
2025-08-06 13:03:59 -0400

    title

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -8,21 +8,7 @@ class Program
     static List<Video> videos = new List<Video>();
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
-        //escribe un programa que cree de 3 a 4 videos, establezca los valores adecuados y, para cada uno, 
-        // agregue una lista de 3 a 4 comentarios (con el nombre y el texto del comentarista). 
-        // Coloca cada uno de estos videos en una lista. 
-        // Si no puedo ingresarlos. Que se generen al azar de una lista de comentarios, usuarios y nombres de videos. 
-        // Las posibilidades son diversas
-
-        //class Video (registre el título, el autor y la duración (en segundos) del video.)
-        //Cada video también debe almacenar una lista de comentarios y un método para devolver el número de comentarios. 
-
-        //class comment  registre tanto el nombre de la persona que lo comentó como su texto.
-
-        //Luego, haz que tu programa itere por la lista de videos y, para cada uno, muestre el título, el autor, la duración 
-        // y el número de comentarios (del método) y, finalmente, enumere todos los comentarios de ese video. Repite esta 
-        // visualización para cada video de la lista. No necesitas que se llame a un video en particular, solo que el programa muestre todos los datos
+        Console.WriteLine("YouTubeVideos Project.");
 
         Video video1 = new Video();
         {
